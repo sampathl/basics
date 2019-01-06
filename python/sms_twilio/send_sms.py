dce6e36cf5d85107a4537c3c99a71fc12befd36f
sampathl <lakkaraju.sampath@outlook.com>
2019-01-06 11:46:39 -0800

    corrected sms_twilio.py for number entries

--- python/sms_twilio/send_sms.py
+++ python/sms_twilio/send_sms.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "AC2a9e1c151fa57e1a35aa3e9e03794538"
+account_sid = "AC2a9e1c151fa57e1aXXXXXXXX"
 # Your Auth Token from twilio.com/console
-auth_token  = "a930e6e4310c84e71c086b0cabde852e"
+auth_token  = "XXXXXXXXXXXXXXX"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+14086936142", 
+    to="+1XXXXXXXXXX", 
     from_="+18317038510",
     body="Hello from Python!")
 
